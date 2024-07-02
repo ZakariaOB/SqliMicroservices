8b0964bae01d4aa99d76da3afa03576cf8021b5f
ZakariaB <boukhriszakaria@gmail.com>
2024-07-02 16:48:08 +0100

    Add the hello proto

--- src/SqliEcommerce/Discount.Grpc/Services/DiscountService.cs
+++ src/SqliEcommerce/Discount.Grpc/Services/DiscountService.cs
@@ -17,7 +17,9 @@ public class DiscountService
             .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
 
         if (coupon is null)
+        {
             coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+        }
 
         logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
 
@@ -29,7 +31,9 @@ public class DiscountService
     {
         var coupon = request.Coupon.Adapt<Coupon>();
         if (coupon is null)
+        {
             throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request object."));
+        }
 
         dbContext.Coupons.Add(coupon);
         await dbContext.SaveChangesAsync();
@@ -45,7 +49,9 @@ public class DiscountService
     {
         var coupon = request.Coupon.Adapt<Coupon>();
         if (coupon is null)
+        {
             throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request object."));
+        }
 
         dbContext.Coupons.Update(coupon);
         await dbContext.SaveChangesAsync();
@@ -63,7 +69,9 @@ public class DiscountService
             .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
 
         if (coupon is null)
+        {
             throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName} is not found."));
+        }
 
         dbContext.Coupons.Remove(coupon);
         await dbContext.SaveChangesAsync();
---
+++ src/SqliEcommerce/Discount.Grpc/Services/HelloService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using helloGrpc;
+
+namespace Discount.Grpc.Services
+{
+    public class HelloService : HelloProtoService.HelloProtoServiceBase
+    {
+        public override Task<HelloReply> SayHello(
+            HelloRequest request, 
+            ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = $"Hello, {request.Name}!"
+            });
+        }
+    }
+}
